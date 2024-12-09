3888707aafb673106f7d1800249de26a1755bec4
Jolly Chen <jolly.chen@student.uva.nl>
2024-12-09 14:46:01 +0100

    Change benchmark operations
    - Renamed MulAdd and SqrtLog to MemoryBound and ComputeBound,
      respectively
    - Removed MoreMulAdd
    - Changed operations in SqrtLog/ComputeBound to a bunch of
      additions, subtractions, multiplications, and divisions
      because std::sqrt and std::log are not autovectorized by
      the gcc compiler

--- motivation/aos2soa.cpp
+++ motivation/aos2soa.cpp
@@ -19,7 +19,7 @@ constexpr size_t container_size = 10e6;
  */
 
 template <typename Container>
-static void BM_MulAddAoS(benchmark::State &state) {
+static void BM_MemoryBoundAoS(benchmark::State &state) {
   Container data;
   auto &v1 = data.v1;
   auto &v2 = data.v2;
@@ -44,7 +44,7 @@ static void BM_MulAddAoS(benchmark::State &state) {
 }
 
 template <typename Container>
-static void BM_MoreMulAddAoS(benchmark::State &state) {
+static void BM_ComputeBoundAoS(benchmark::State &state) {
   Container data;
   auto &v1 = data.v1;
   auto &v2 = data.v2;
@@ -53,41 +53,26 @@ static void BM_MoreMulAddAoS(benchmark::State &state) {
   for (const auto &&_ : state) {
     auto start = Clock::now();
     std::transform(v1.begin(), v1.end(), v2.begin(), out.begin(), [](const auto &s1, const auto &s2) {
-      return (s2.x1 - s1.x2) * (s2.x2 - s1.x1) + // cross
-             (s2.x2 - s1.x1) * (s2.x1 - s1.x2) + // cross
-             (s2.x1 - s1.x1) * (s2.x1 - s1.x1) + // sqaure
-             (s2.x2 - s1.x2) * (s2.x2 - s1.x2);  // square
+      return (s2.x1 - s1.x2) * (s2.x2 - s1.x1) + (s2.x2 - s1.x1) * (s2.x1 - s1.x2) + (s2.x1 + s1.x2) * (s2.x2 - s1.x1) +
+             (s2.x2 - s1.x1) * (s2.x1 + s1.x2) + (s2.x1 + s1.x2) * (s2.x2 + s1.x1) + (s2.x2 + s1.x1) * (s2.x1 + s1.x2) +
+             (s2.x1 - s1.x1) * (s2.x1 - s1.x1) + (s2.x2 - s1.x2) * (s2.x2 - s1.x2) + (s2.x1 + s1.x1) * (s2.x1 + s1.x1) +
+             (s2.x2 + s1.x2) * (s2.x2 + s1.x2) + (s2.x1 - s1.x1) * (s2.x1 - s1.x1) - (s2.x1 - s1.x2) / (s2.x2 - s1.x1) -
+             (s2.x2 - s1.x1) / (s2.x1 - s1.x2) - (s2.x1 + s1.x2) / (s2.x2 - s1.x1) - (s2.x2 - s1.x1) / (s2.x1 + s1.x2) -
+             (s2.x1 + s1.x2) / (s2.x2 + s1.x1) - (s2.x2 + s1.x1) / (s2.x1 + s1.x2) - (s2.x1 - s1.x1) / (s2.x1 - s1.x1) -
+             (s2.x2 - s1.x2) / (s2.x2 - s1.x2) - (s2.x1 + s1.x1) / (s2.x1 + s1.x1) - (s2.x2 + s1.x2) / (s2.x2 + s1.x2) -
+             (s2.x1 * s1.x1) - (s2.x1 * s1.x1) + (s2.x1 * s1.x2) - (s2.x2 * s1.x1) + (s2.x2 * s1.x1) - (s2.x1 * s1.x2) +
+             (s2.x1 * s1.x2) - (s2.x2 * s1.x1) + (s2.x2 * s1.x1) - (s2.x1 * s1.x2) + (s2.x1 * s1.x2) - (s2.x2 * s1.x1) +
+             (s2.x2 * s1.x1) - (s2.x1 * s1.x2) + (s2.x1 * s1.x1) - (s2.x1 * s1.x1) + (s2.x2 * s1.x2) - (s2.x2 * s1.x2) +
+             (s2.x1 * s1.x1) - (s2.x1 * s1.x1) + (s2.x2 / s1.x2) + (s2.x2 / s1.x2) - (s2.x1 / s1.x1) + (s2.x1 / s1.x1) -
+             (s2.x1 / s1.x2) + (s2.x2 / s1.x1) - (s2.x2 / s1.x1) + (s2.x1 / s1.x2) - (s2.x1 / s1.x2) + (s2.x2 / s1.x1) -
+             (s2.x2 / s1.x1) + (s2.x1 / s1.x2) - (s2.x1 / s1.x2) + (s2.x2 / s1.x1) - (s2.x2 / s1.x1) + (s2.x1 / s1.x2) -
+             (s2.x1 / s1.x1) + (s2.x1 / s1.x1) - (s2.x2 / s1.x2) + (s2.x2 / s1.x2) - (s2.x1 / s1.x1) + (s2.x1 / s1.x1) -
+             (s2.x2 / s1.x2) + (s2.x2 / s1.x2);
     });
     auto end = Clock::now();
 
     auto elapsed_seconds = std::chrono::duration_cast<fsecs>(end - start);
     state.SetIterationTime(elapsed_seconds.count());
-  }
-
-  state.counters["size"] = out.size();
-}
-
-template <typename Container>
-static void BM_SqrtLogAoS(benchmark::State &state) {
-  Container data;
-  auto &v1 = data.v1;
-  auto &v2 = data.v2;
-  auto &out = data.out;
-
-  for (const auto &&_ : state) {
-    auto start = Clock::now();
-    std::transform(v1.begin(), v1.end(), v2.begin(), out.begin(), [](const auto &s1, const auto &s2) {
-      return (std::sqrt(s2.x1) - std::log(s1.x1)) * (std::sqrt(s2.x1) - std::log(s1.x1)) +
-             (std::log(s2.x2) - std::sqrt(s1.x2)) * (std::sqrt(s2.x2) - std::sqrt(s1.x2));
-    });
-    auto end = Clock::now();
-
-    auto elapsed_seconds = std::chrono::duration_cast<fsecs>(end - start);
-    state.SetIterationTime(elapsed_seconds.count());
-
-    if (out.size() == 0) {
-      // TODO: check result
-    }
   }
 
   state.counters["size"] = out.size();
@@ -97,7 +82,7 @@ static void BM_SqrtLogAoS(benchmark::State &state) {
  * SoA
  */
 template <class Container>
-static void BM_MulAddSoA(benchmark::State &state) {
+static void BM_MemoryBoundSoA(benchmark::State &state) {
   Container data;
   auto deltaR2 = [](const auto &v1_x1, const auto &v2_x1, const auto &v1_x2, const auto &v2_x2) {
     return (v2_x1 - v1_x1) * (v2_x1 - v1_x1) + (v2_x2 - v1_x2) * (v2_x2 - v1_x2);
@@ -126,13 +111,24 @@ static void BM_MulAddSoA(benchmark::State &state) {
 }
 
 template <class Container>
-static void BM_MoreMulAddSoA(benchmark::State &state) {
+static void BM_ComputeBoundSoA(benchmark::State &state) {
   Container data;
   auto deltaR2 = [](const auto &v1_x1, const auto &v2_x1, const auto &v1_x2, const auto &v2_x2) {
-    return (v2_x1 - v1_x2) * (v2_x2 - v1_x1) + // cross
-           (v2_x2 - v1_x1) * (v2_x1 - v1_x2) + // cross
-           (v2_x1 - v1_x1) * (v2_x1 - v1_x1) + // square
-           (v2_x2 - v1_x2) * (v2_x2 - v1_x2);  // square
+    return (v2_x1 - v1_x2) * (v2_x2 - v1_x1) + (v2_x2 - v1_x1) * (v2_x1 - v1_x2) + (v2_x1 + v1_x2) * (v2_x2 - v1_x1) +
+           (v2_x2 - v1_x1) * (v2_x1 + v1_x2) + (v2_x1 + v1_x2) * (v2_x2 + v1_x1) + (v2_x2 + v1_x1) * (v2_x1 + v1_x2) +
+           (v2_x1 - v1_x1) * (v2_x1 - v1_x1) + (v2_x2 - v1_x2) * (v2_x2 - v1_x2) + (v2_x1 + v1_x1) * (v2_x1 + v1_x1) +
+           (v2_x2 + v1_x2) * (v2_x2 + v1_x2) + (v2_x1 - v1_x1) * (v2_x1 - v1_x1) - (v2_x1 - v1_x2) / (v2_x2 - v1_x1) -
+           (v2_x2 - v1_x1) / (v2_x1 - v1_x2) - (v2_x1 + v1_x2) / (v2_x2 - v1_x1) - (v2_x2 - v1_x1) / (v2_x1 + v1_x2) -
+           (v2_x1 + v1_x2) / (v2_x2 + v1_x1) - (v2_x2 + v1_x1) / (v2_x1 + v1_x2) - (v2_x1 - v1_x1) / (v2_x1 - v1_x1) -
+           (v2_x2 - v1_x2) / (v2_x2 - v1_x2) - (v2_x1 + v1_x1) / (v2_x1 + v1_x1) - (v2_x2 + v1_x2) / (v2_x2 + v1_x2) -
+           (v2_x1 * v1_x1) - (v2_x1 * v1_x1) + (v2_x1 * v1_x2) - (v2_x2 * v1_x1) + (v2_x2 * v1_x1) - (v2_x1 * v1_x2) +
+           (v2_x1 * v1_x2) - (v2_x2 * v1_x1) + (v2_x2 * v1_x1) - (v2_x1 * v1_x2) + (v2_x1 * v1_x2) - (v2_x2 * v1_x1) +
+           (v2_x2 * v1_x1) - (v2_x1 * v1_x2) + (v2_x1 * v1_x1) - (v2_x1 * v1_x1) + (v2_x2 * v1_x2) - (v2_x2 * v1_x2) +
+           (v2_x1 * v1_x1) - (v2_x1 * v1_x1) + (v2_x2 / v1_x2) + (v2_x2 / v1_x2) - (v2_x1 / v1_x1) + (v2_x1 / v1_x1) -
+           (v2_x1 / v1_x2) + (v2_x2 / v1_x1) - (v2_x2 / v1_x1) + (v2_x1 / v1_x2) - (v2_x1 / v1_x2) + (v2_x2 / v1_x1) -
+           (v2_x2 / v1_x1) + (v2_x1 / v1_x2) - (v2_x1 / v1_x2) + (v2_x2 / v1_x1) - (v2_x2 / v1_x1) + (v2_x1 / v1_x2) -
+           (v2_x1 / v1_x1) + (v2_x1 / v1_x1) - (v2_x2 / v1_x2) + (v2_x2 / v1_x2) - (v2_x1 / v1_x1) + (v2_x1 / v1_x1) -
+           (v2_x2 / v1_x2) + (v2_x2 / v1_x2);
   };
 
   auto &v1_x1 = data.v1_x1;
@@ -148,36 +144,6 @@ static void BM_MoreMulAddSoA(benchmark::State &state) {
 
     auto elapsed_seconds = std::chrono::duration_cast<fsecs>(end - start);
     state.SetIterationTime(elapsed_seconds.count());
-  }
-
-  state.counters["size"] = data.out.size();
-}
-
-template <class Container>
-static void BM_SqrtLogSoA(benchmark::State &state) {
-  Container data;
-  auto deltaR2 = [](const auto &v1_x1, const auto &v2_x1, const auto &v1_x2, const auto &v2_x2) {
-    return (std::sqrt(v2_x1) - std::log(v1_x1)) * (std::sqrt(v2_x1) - std::log(v1_x1)) +
-           (std::log(v2_x2) - std::sqrt(v1_x2)) * (std::sqrt(v2_x2) - std::sqrt(v1_x2));
-  };
-
-  auto &v1_x1 = data.v1_x1;
-  auto &v2_x1 = data.v2_x1;
-  auto &v1_x2 = data.v1_x2;
-  auto &v2_x2 = data.v2_x2;
-  auto &out = data.out;
-
-  for (const auto &&_ : state) {
-    auto start = Clock::now();
-    std::ranges::copy(std::views::zip_transform(deltaR2, v1_x1, v2_x1, v1_x2, v2_x2), out.begin());
-    auto end = Clock::now();
-
-    auto elapsed_seconds = std::chrono::duration_cast<fsecs>(end - start);
-    state.SetIterationTime(elapsed_seconds.count());
-
-    if (out.size() == 0) {
-      // TODO: check result
-    }
   }
 
   state.counters["size"] = data.out.size();
@@ -186,32 +152,28 @@ static void BM_SqrtLogSoA(benchmark::State &state) {
 #define BENCHMARK_ALL_TYPES(OP, MEMBERS, TYPE, SIZE)                                                                   \
   BENCHMARK_TEMPLATE(BM_##OP##AoS, AoS##MEMBERS##Raw<TYPE, SIZE>)->Unit(benchmark::kMillisecond)->UseManualTime();     \
   BENCHMARK_TEMPLATE(BM_##OP##SoA, SoA##MEMBERS##Raw<TYPE, SIZE>)->Unit(benchmark::kMillisecond)->UseManualTime();     \
-  BENCHMARK_TEMPLATE(BM_##OP##AoS, AoS##MEMBERS##Arr<TYPE, container_size>)                                                \
+  BENCHMARK_TEMPLATE(BM_##OP##AoS, AoS##MEMBERS##Arr<TYPE, container_size>)                                            \
       ->Unit(benchmark::kMillisecond)                                                                                  \
       ->UseManualTime();                                                                                               \
-  BENCHMARK_TEMPLATE(BM_##OP##SoA, SoA##MEMBERS##Arr<TYPE, container_size>)                                                \
+  BENCHMARK_TEMPLATE(BM_##OP##SoA, SoA##MEMBERS##Arr<TYPE, container_size>)                                            \
       ->Unit(benchmark::kMillisecond)                                                                                  \
       ->UseManualTime();                                                                                               \
-  BENCHMARK_TEMPLATE(BM_##OP##AoS, AoS##MEMBERS##Vec<TYPE, container_size>)                                               \
+  BENCHMARK_TEMPLATE(BM_##OP##AoS, AoS##MEMBERS##Vec<TYPE, container_size>)                                            \
       ->Unit(benchmark::kMillisecond)                                                                                  \
       ->UseManualTime();                                                                                               \
-  BENCHMARK_TEMPLATE(BM_##OP##SoA, SoA##MEMBERS##Vec<TYPE, container_size>)                                               \
+  BENCHMARK_TEMPLATE(BM_##OP##SoA, SoA##MEMBERS##Vec<TYPE, container_size>)                                            \
       ->Unit(benchmark::kMillisecond)                                                                                  \
       ->UseManualTime();
 
 #define BENCHMARK_ALL_MEMBERS(TYPE, SIZE)                                                                              \
-  BENCHMARK_ALL_TYPES(MulAdd, 2, TYPE, SIZE)                                                                           \
-  BENCHMARK_ALL_TYPES(MulAdd, 4, TYPE, SIZE)                                                                           \
-  BENCHMARK_ALL_TYPES(MulAdd, 8, TYPE, SIZE)                                                                           \
-  BENCHMARK_ALL_TYPES(MulAdd, 20, TYPE, SIZE)                                                                          \
-  BENCHMARK_ALL_TYPES(MoreMulAdd, 2, TYPE, SIZE)                                                                       \
-  BENCHMARK_ALL_TYPES(MoreMulAdd, 4, TYPE, SIZE)                                                                       \
-  BENCHMARK_ALL_TYPES(MoreMulAdd, 8, TYPE, SIZE)                                                                       \
-  BENCHMARK_ALL_TYPES(MoreMulAdd, 20, TYPE, SIZE)                                                                      \
-  BENCHMARK_ALL_TYPES(SqrtLog, 2, TYPE, SIZE)                                                                          \
-  BENCHMARK_ALL_TYPES(SqrtLog, 4, TYPE, SIZE)                                                                          \
-  BENCHMARK_ALL_TYPES(SqrtLog, 8, TYPE, SIZE)                                                                          \
-  BENCHMARK_ALL_TYPES(SqrtLog, 20, TYPE, SIZE)
+  BENCHMARK_ALL_TYPES(MemoryBound, 2, TYPE, SIZE)                                                                      \
+  BENCHMARK_ALL_TYPES(MemoryBound, 4, TYPE, SIZE)                                                                      \
+  BENCHMARK_ALL_TYPES(MemoryBound, 8, TYPE, SIZE)                                                                      \
+  BENCHMARK_ALL_TYPES(MemoryBound, 20, TYPE, SIZE)                                                                     \
+  BENCHMARK_ALL_TYPES(ComputeBound, 2, TYPE, SIZE)                                                                     \
+  BENCHMARK_ALL_TYPES(ComputeBound, 4, TYPE, SIZE)                                                                     \
+  BENCHMARK_ALL_TYPES(ComputeBound, 8, TYPE, SIZE)                                                                     \
+  BENCHMARK_ALL_TYPES(ComputeBound, 20, TYPE, SIZE)
 
 BENCHMARK_ALL_MEMBERS(float, container_size)
 BENCHMARK_ALL_MEMBERS(double, container_size)
