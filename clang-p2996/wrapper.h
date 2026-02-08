d7b5fe872aa8ebeaadfd50e4647e6f668fe8ff28
Jolly Chen <jolly.chen@cern.ch>
2026-02-08 17:06:15 +0100

    Put wrapper generator in header file

---
+++ clang-p2996/wrapper.h
@@ -0,0 +1,138 @@
+/**
+ * This example is adapted from Oliver Rietmann's SoA wrapper approach:
+ *     https://github.com/cern-nextgen/wp1.7-soa-wrapper/commit/21d5b849ef7336bd56ebd896c9f34339a5aa75e3
+ * This version reduces code duplication by using C++26 reflection features.
+ */
+
+#include <experimental/meta>
+#include <ranges>
+
+template <class T>
+using value_type = T;
+
+template <class T>
+using reference = T &;
+
+template <class T>
+using const_reference = const T &;
+
+//////////////// Reflection utilities
+
+template <class S>
+constexpr std::size_t count_members() {
+  return nonstatic_data_members_of(^^S, std::meta::access_context::unchecked()).size();
+}
+
+consteval auto nsdms(std::meta::info type) -> std::vector<std::meta::info> {
+  return nonstatic_data_members_of(type, std::meta::access_context::unchecked());
+}
+
+template <class F>
+consteval auto transform_members(std::meta::info type, F f) {
+  return nsdms(type) | std::views::transform([=](std::meta::info member) {
+           return data_member_spec(f(type_of(member)), {.name = identifier_of(member)});
+         });
+}
+
+namespace __impl {
+template <auto... vals>
+struct replicator_type {
+  template <typename F>
+  constexpr auto operator>>(F body) const -> decltype(auto) {
+    return body.template operator()<vals...>();
+  }
+};
+
+template <auto... vals>
+replicator_type<vals...> replicator = {};
+} // namespace __impl
+
+template <typename R>
+consteval auto expand_all(R range) {
+  std::vector<std::meta::info> args;
+  for (auto r : range) {
+    args.push_back(reflect_constant(r));
+  }
+  return substitute(^^__impl::replicator, args);
+}
+
+//////////////// Wrpapper generator
+
+template <typename S, template <class> class F>
+struct WrapperGenerator {
+  struct Base;
+
+  consteval {
+    define_aggregate(
+        ^^Base, transform_members(^^S, [](std::meta::info type) { return substitute(^^F, {remove_cvref(type)}); }));
+  }
+
+  class Wrapper : public Base {
+
+    template <typename Out, class FunctionObject>
+    Out apply_to_members(Wrapper &s, FunctionObject &&f) {
+      auto construct_output = [&]<size_t... Is>(std::index_sequence<Is...>) -> Out {
+        return {f(s.[:nsdms(^^Base)[Is]:])...};
+      };
+      constexpr auto indices = std::make_index_sequence<count_members<Base>()>{};
+      return construct_output(indices);
+    }
+
+  public:
+    //////// Random Access operators
+
+    S operator[](int i) { return apply_to_members<S>(static_cast<Wrapper &>(*this), evaluate_at{i}); }
+    WrapperGenerator<S, F>::Wrapper operator[](int i) const {
+      return apply_to_members<WrapperGenerator<S, F>::Wrapper>(static_cast<const Wrapper &>(*this), evaluate_at{i});
+    }
+
+    ////// Constructors
+
+    template <typename... T>
+    Wrapper(std::initializer_list<T>... args) : Base(args...) {}
+
+    template <typename... T>
+    Wrapper(T &&...t) : Base(std::forward<T>(t)...) {}
+
+    Wrapper(const Base &b) : Base(b) {}
+
+    ////// Conversion constructors
+    template <typename T>
+      requires(parent_of(^^T) != parent_of(^^Wrapper))
+    Wrapper(T &other)
+        : Wrapper([:expand_all(nsdms(type_of(bases_of(^^T, std::meta::access_context::unchecked())[0]))
+                               ):] >> [&]<auto... members> { return Base{other.[:members:]...}; }) {}
+
+    template <typename T>
+      requires(parent_of(^^T) != parent_of(^^Wrapper))
+    Wrapper(const T &other)
+        : Wrapper([:expand_all(nsdms(type_of(bases_of(^^T, std::meta::access_context::unchecked())[0]))
+                               ):] >> [&]<auto... members> { return Base{other.[:members:]...}; }) {}
+
+  private:
+    //////// Helper
+
+    template <template <class> class F_out>
+    struct cast {
+      template <class T>
+      F_out<T> operator()(F<T> &member) const {
+        return member;
+      }
+    };
+
+    struct evaluate_at {
+      int i;
+      template <class T>
+      reference<T> operator()(F<T> &member) const {
+        return member[i];
+      }
+      template <class T>
+      const_reference<T> operator()(const F<T> &member) const {
+        return member[i];
+      }
+    };
+  };
+};
+
+template <typename S, template <class> class F>
+using Wrapper = WrapperGenerator<S, F>::Wrapper;
